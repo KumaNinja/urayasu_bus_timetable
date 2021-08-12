37e9816e6f13d7f34f6fd7ccceb7044e5a4d1faf
KumaNinja <88824938+KumaNinja@users.noreply.github.com>
2021-08-13 01:25:33 +0900

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from urayasubustimetable import views
-import urayasubustimetable.views as timetable
- 
-urlpatterns = [
-    path('', timetable.index, name='index'),
-    path('test', views.test, name="test"),
-]
